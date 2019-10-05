207e83538284ae3a2e4de3b468a868898e5b0946
Junior Veroneze <jrveroneze@gmail.com>
2019-10-05 15:14:12 -0300

    Insere arquivo da aula sobre variáveis alternativas, sem tipagem definida.

---
+++ variaveis/alternatives.dart
@@ -0,0 +1,18 @@
+void main() {
+	var nome;
+	nome = 'Junior';
+	print(nome);
+
+	// var idade;
+	// idade = 24;
+	// print(idade);
+
+	// var list = [
+	// 	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Feijão', 1.50, true
+	// ];
+	// print(list);
+
+	dynamic idade;
+	idade = 24.00;
+	print(idade);
+}
