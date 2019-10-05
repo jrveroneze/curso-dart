85e929b76148ede0966a04a55a79a606610ef190
Junior Veroneze <jrveroneze@gmail.com>
2019-10-05 14:31:51 -0300

    Insere arquivo da aula sobre lista do tipo Map.

---
+++ variaveis/list-map.dart
@@ -0,0 +1,13 @@
+void main() {
+	Map usuario = {
+		'nome': 'Junior',
+		'sobrenome': 'Veroneze',
+		'idade': 24,
+		'altura': 1.94,
+		'perfil': 'jrveroneze',
+		'perfilIsValid': true
+	};
+
+	print(usuario);
+	print(usuario.length);
+}
