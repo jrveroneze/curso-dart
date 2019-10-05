907b42058696141723d7ad39327fb20fc747e93e
Junior Veroneze <jrveroneze@gmail.com>
2019-10-05 13:48:46 -0300

    Inclui itens da aula de variáveis basicas.

---
+++ variaveis/variaveis.dart
@@ -0,0 +1,18 @@
+void main() {
+	// Variavel de caracter (String)
+	String nome = 'Junior';
+	// nome = '${nome} Veroneze'; // Exemplo de reescrita de variaveis
+	print(nome);
+
+	// Variavel de inteiros (int)
+	int idade = 24;
+	print('Sua idade é: ${idade}');
+
+	// Variavel de valores fracionados (Double)
+	double altura = 1.94;
+	print('Sua altura é: ${altura}');
+
+	// Variavel booleana (Bool)
+	bool juniorDev = true;
+	print(juniorDev);
+}
