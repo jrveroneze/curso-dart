7c78571f4c7d03ebf4e2cd59a0d7a06ea8907c3f
Junior Veroneze <jrveroneze@gmail.com>
2019-11-13 17:53:08 -0300

    Insere arquivo das aulas sobre loop com for, while e do while.

---
+++ controles/loop.dart
@@ -0,0 +1,34 @@
+void main() {
+	// FOR
+	// for (int i = 0; i < 10; i++) {
+	// 	print(i);
+	// 	print(i * 2);
+	// 	print(i / 2);
+	// 	print(i - 2);
+	// 	print(i + 2 * 3);
+	// }
+
+	// List listaProdutos = [
+	// 	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Feijão', 1.50, true
+	// ];
+
+	// for (int i = 0; i < listaProdutos.length; i++) {
+	// 	print(listaProdutos);
+	// }
+
+	// for (var item in listaProdutos) {
+	// 	print(item);
+	// }
+
+	// WHILE
+	int idade = 10;
+	while(idade < 18) {
+		print(idade.toString() + ' é menor que 18 anos');
+		idade++;
+	}
+
+	// DO WHILE
+	do {
+		print('Teste de while');
+	} while (false);
+}
